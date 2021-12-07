188bbaf43b490d96e6a64012c13a61de81ef3b2f
hamedMatari <matari.hamed@yahoo.com>
2021-12-07 20:50:20 +0330

    Layout,Header,Footer

---
+++ components/Footer.jsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import styles from "../styles/Footer.module.css";
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p>Copyrights &copy ; developet by hamed</p>
+      <p>
+        <Link href="/about">About this project</Link>
+      </p>
+    </footer>
+  );
+}
--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Header from "./Header";
+import Footer from "./Footer";
 import styles from "../styles/Layout.module.css";
 export default function Layout({ title, keywords, description, children }) {
   return (
@@ -11,6 +12,7 @@ export default function Layout({ title, keywords, description, children }) {
       </Head>
       <Header />
       <div className={styles.container}>{children}</div>
+      <Footer />
     </div>
   );
 }
